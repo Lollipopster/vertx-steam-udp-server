455024d18e42f9836cba0c97b37502f8c100aa91
strogiyotec <almas337519@gmail.com>
2018-05-13 18:05:29 +0600

    change substring to regex in Parsed body

--- src/main/java/com/udp/server/parser/GameEventAction.java
+++ src/main/java/com/udp/server/parser/GameEventAction.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author strogiyotec
@@ -15,15 +18,25 @@ import org.springframework.stereotype.Component;
 @ParserQualifier(type = ParserQualifier.ParserType.GameEvent)
 @Slf4j
 public final class GameEventAction implements ParsedLogBody{
-
+    private static final String STEAM_ID_PATTERN = "STEAM.*[0-9]";
     @Override
     public JsonObject get(final String body) {
         if(body.contains("BOT")){
             return null;
         }
-        final String startFromSteamId = body.substring(body.indexOf("STEAM"),body.length());
-        final String onlySteamId = startFromSteamId.substring(0,startFromSteamId.indexOf(">"));
-        return new JsonObject().put("steamId",onlySteamId);
+        final String steamId = this.steamIdFromBody(body);
+        return new JsonObject().put("steamId",steamId);
     }
-    
+
+    private String steamIdFromBody(final String body) {
+        final Pattern pattern = Pattern.compile(GameEventAction.STEAM_ID_PATTERN);
+        final Matcher matcher = pattern.matcher(body);
+        if(matcher.find()){
+            return matcher.group();
+        } else{
+            throw new IllegalArgumentException(String.format("Body %s doesn't has steam id ",body));
+        }
+
+    }
+
 }
