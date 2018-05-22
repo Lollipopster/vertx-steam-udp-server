c0a0e4d11c3940e55b4347097cd2a19f9cfd4055
strogiyotec <almas337519@gmail.com>
2018-05-22 13:28:54 +0600

    change pattern for steam id

--- src/main/java/com/udp/server/parser/GameEventAction.java
+++ src/main/java/com/udp/server/parser/GameEventAction.java
@@ -18,7 +18,7 @@ import java.util.regex.Pattern;
 @ParserQualifier(type = ParserQualifier.ParserType.GameEvent)
 @Slf4j
 public final class GameEventAction implements ParsedLogBody{
-    private static final String STEAM_ID_PATTERN = "STEAM_1:1:\\d{13}";
+    private static final String STEAM_ID_PATTERN = "STEAM_1:1:|STEAM_1:0:\\d{7,13}";
     @Override
     public JsonObject get(final String body) {
         if(body.contains("BOT")){
